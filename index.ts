97860e6958ab98d69847f17186972c14d2aa4b1b
Jonathan Laukenmann <jonathan.laukenmann@formbench.com>
2021-10-08 23:06:44 +0200

    feat: explicitely export types in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,40 @@
 import type { BillbeeResponse } from "./types/BillbeeResponse";
-import type { BillbeeCustomer } from "./types/BillbeeCustomer";
+import type { BillbeeCustomer, BillbeeCustomerAddress } from "./types/BillbeeCustomer";
+import type { BillbeeOrder, StockDictionary, BillbeeOrderStates } from "./types/BillbeeOrder";
+import type {
+  BillbeeProduct,
+  ReservedAmount,
+  CustomField,
+  ProductImage,
+  UpdateStockResponse,
+  ImagesDeleteResponse
+} from "./types/BillbeeProduct";
+import type { BillbeeSearchResults } from "./types/BillbeeSearch";
+import type {
+  ShippingProvider,
+  ShippingProviderProduct,
+  ShippingCarrier,
+  ShipmentsResponse,
+  ShipWithLabelResponse
+} from "./types/BillbeeShipments";
 
-export type { BillbeeResponse, BillbeeCustomer };
+export type {
+  BillbeeResponse,
+  BillbeeCustomer,
+  BillbeeCustomerAddress,
+  BillbeeOrder,
+  StockDictionary,
+  BillbeeOrderStates,
+  BillbeeProduct,
+  ReservedAmount,
+  CustomField,
+  ProductImage,
+  UpdateStockResponse,
+  ImagesDeleteResponse,
+  BillbeeSearchResults,
+  ShippingProvider,
+  ShippingProviderProduct,
+  ShippingCarrier,
+  ShipmentsResponse,
+  ShipWithLabelResponse
+};
--- types/BillbeeHeaders.ts
+++
@@ -1,7 +0,0 @@
-import { HeadersInit } from "node-fetch";
-
-// TODO
-export type BillbeeHeaders = HeadersInit & {
-  Authorization: string;
-  "X-Billbee-Api-Key": string;
-};
