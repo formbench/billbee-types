7824a8f19a25c4346b1fb8f707ab813f71cd112b
Jonathan Laukenmann <jonathan.laukenmann@formbench.com>
2022-03-25 18:32:06 +0100

    feat: add auto publish functionality

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -55,6 +55,7 @@ if (!choices.includes(versionType)) {
   await $`yarn version --new-version ${next} --no-git-tag-version`;
   await $`git add package.json && git commit --amend --no-edit`;
   await $`git push --force-with-lease`;
+  await $`npm publish`;
 
   await $`gh release create v${next}`;
   console.log('');
