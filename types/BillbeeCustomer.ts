163a3b7833ea94ed5527c4f0c662b60909d26ab5
Namekkural <jonathan.laukenmann@formbench.com>
2024-02-22 15:13:38 +0100

    feat: update customer tpes

--- types/BillbeeCustomer.ts
+++ types/BillbeeCustomer.ts
@@ -1,3 +1,11 @@
+export interface BillbeeCustomerContactData {
+  Id: number;
+  TypeId: number;
+  TypeName: string;
+  SubType: string;
+  Value: string;
+}
+
 export interface BillbeeCustomer {
   Id: number;
   Name: string;
@@ -7,6 +15,13 @@ export interface BillbeeCustomer {
   Number: number;
   PriceGroupId?: number;
   LanguageId?: number;
+  DefaultMailAddress?: BillbeeCustomerContactData;
+  DefaultCommercialMailAddress?: BillbeeCustomerContactData;
+  DefaultStatusUpdatesMailAddress?: BillbeeCustomerContactData;
+  DefaultPhone1?: BillbeeCustomerContactData;
+  DefaultPhone2?: BillbeeCustomerContactData;
+  DefaultFax?: BillbeeCustomerContactData;
   VatId?: string;
   Type?: number;
+  MetaData?: BillbeeCustomerContactData[];
 }
